a3854e577401b19e2caba1fee0d358d6981f36c7
Zgoo <jizhongchun@gmail.com>
2018-03-27 21:05:52 +0800

    change style to bootstrap

--- type_game/js/app.js
+++ type_game/js/app.js
@@ -1,65 +1,69 @@
- Vue.config.keyCode = {
-    F1: 112
-  };
-  var vm = new Vue({
-    el: '#app',
-    data: {
-      active: { },
-      keyCodeAlias: {
-        '`': 'Grave',
-        '~': 'Tilde',
-        1: 'One',
-        '!': 'Exclamation',
-        2: 'Two',
-        '@': 'AtSign',
-        3: 'Three',
-        '#': 'Number',
-        4: 'Four',
-        '$': 'Dollar',
-        5: 'Five',
-        '%': 'Percent',
-        6: 'Six',
-        '^': 'Caret',
-        7: 'Seven',
-        '&': 'Ampersand',
-        8: 'Eight',
-        '*': 'Asterisk',
-        9: 'Nine',
-        '(': 'LeftParenthesis',
-        0: 'Zero',
-        ')': 'RightParenthesis',
-        '-': 'Minus',
-        '_': 'Underscore',
-        '=': 'EqualitySign',
-        '+': 'Plus',
+Vue.config.keyCode = {
+  F1: 112
+};
+var vm = new Vue({
+  el: '#app',
+  data: {
+    active: {},
+    keyCodeAlias: {
+      '`': 'Grave',
+      '~': 'Tilde',
+      1: 'One',
+      '!': 'Exclamation',
+      2: 'Two',
+      '@': 'AtSign',
+      3: 'Three',
+      '#': 'Number',
+      4: 'Four',
+      '$': 'Dollar',
+      5: 'Five',
+      '%': 'Percent',
+      6: 'Six',
+      '^': 'Caret',
+      7: 'Seven',
+      '&': 'Ampersand',
+      8: 'Eight',
+      '*': 'Asterisk',
+      9: 'Nine',
+      '(': 'LeftParenthesis',
+      0: 'Zero',
+      ')': 'RightParenthesis',
+      '-': 'Minus',
+      '_': 'Underscore',
+      '=': 'EqualitySign',
+      '+': 'Plus',
 
-        '{': 'LeftSquareBracket',
-        '[': 'LeftCurlyBracket',
-        '}': 'RightSquareBracket',
-        ']': 'RightCurlyBracket',
-        '|': 'VertialBar',
-        '\\': 'Backslash',
+      '{': 'LeftSquareBracket',
+      '[': 'LeftCurlyBracket',
+      '}': 'RightSquareBracket',
+      ']': 'RightCurlyBracket',
+      '|': 'VerticalBar',
+      '\\': 'Backslash',
 
-        ':': 'Colon',
-        ';': 'Semicolon',
-        '\"': 'DoubleQuote',
-        '\'': 'SingleQuote',
+      ':': 'Colon',
+      ';': 'Semicolon',
+      '\"': 'DoubleQuote',
+      '\'': 'SingleQuote',
 
-        '<': 'LessThan',
-        ',': 'Comma',
-        '>': 'GreaterThan',
-        '.': 'Period',
-        '?': 'QuestionMark',
-        '/': 'Slash',
-      }
-    },
-    methods: {
-      checkUserKeyDown: function (event) {
-        console.log(event.key);
-        this.$set(this.active, this.keyCodeAlias[event.key] || event.key, true)
-      },
-      checkUserKeyUp: function (event) {
-        this.$set(this.active, this.keyCodeAlias[event.key] || event.key, false)
-      }
+      '<': 'LessThan',
+      ',': 'Comma',
+      '>': 'GreaterThan',
+      '.': 'Period',
+      '?': 'QuestionMark',
+      '/': 'Slash',
+
+      'space': 'Space'
     }
-  });
+  },
+  methods: {
+    checkUserKeyDown: function (event) {
+      var key = event.keyCode === 32 ? 'space' : event.key;
+      console.log(event.keyCode);
+      this.$set(this.active, this.keyCodeAlias[key] || key, true)
+    },
+    checkUserKeyUp: function (event) {
+      var key = event.keyCode === 32 ? 'space' : event.key;
+      this.$set(this.active, this.keyCodeAlias[key] || key, false)
+    }
+  }
+});
